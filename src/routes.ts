c22ed39169e95b1dcef95bc1a8738eef8ac449f6
KLusvarghi <kauaolusvarghi@gmail.com>
2025-05-12 19:54:53 -0300

    chore(swagger) add doc from swagger

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,34 @@ import { verifyToken } from "./auth/verify.middleware";
 
 const routes = Router();
 
+/**
+ * @swagger
+ * /books:
+ *   post:
+ *     summary: create new books
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Login successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ */
 routes.post("/books", bookController.create);
 routes.get("/books", verifyToken, bookController.find);
 routes.get("/books/:title", bookController.findByTitle);
